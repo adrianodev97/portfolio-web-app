70177ac99a73ed10375041ab29707464c9d34e0b
Adriano Junior <adrianojunior.dev97@gmail.com>
2023-10-05 17:11:38 -0300

    update social media style

--- src/components/MUIHeader/components/MUISocialMedias/styles.ts
+++ src/components/MUIHeader/components/MUISocialMedias/styles.ts
@@ -6,10 +6,10 @@ export const MUIStyledSocialMediasContent = styled(IconButton)<{
   onMobile: boolean
 }>(({ theme, onMobile }) => ({
   margin: '0 auto',
-  transform: 'scale(1.5)',
   paddingTop: onMobile ? theme.spacing(5) : 0,
   [theme.breakpoints.down('md')]: {
     display: onMobile ? 'block' : 'none',
+    transform: 'scale(1.5)',
   },
 }))
 
